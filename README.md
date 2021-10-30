6b5b489b6985e9b3c3ac17dd37024d0e25579f33
Laxminarayen <42375609+Laxminarayen@users.noreply.github.com>
2021-10-30 11:24:15 +0530

    commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flask_heroku_demo
+ 
--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+import flask
+from flask.app import Flask
 
-app = Flask(__name__);
+from flask import Flask, render_template,request
+import pandas as pd
+import joblib
+
+model = joblib.load('pipe.pkl')
+
+app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('index.html')
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/predict",methods = ['POST'])
+def predict():
+    test_pd = pd.DataFrame([request.form])
+    #value = model.predict(test_pd)[0]
+    return request.form
+    '''
+    value = str(round(value,2))
+    return render_template("predict.html",price = value +" Lakhs")
+'''
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
+
+
